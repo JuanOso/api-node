f78b2c6064f8a0377e933d828a2796cbf4cecaa0
Juan Osorio <jcamilo994@hotmail.com>
2022-10-21 21:17:44 -0500

    apply dbUrl

--- libs/postgresPool.js
+++ libs/postgresPool.js
@@ -1,12 +1,17 @@
 const { Pool } = require('pg');
 const { config } = require('./../config/config');
 
-//encode uri es para protejer esta informacion
-const USER = encodeURIComponent(config.dbUser);
-const PASSWORD = encodeURIComponent(config.dbPassword);
-//usualmente la ruta que nos da un servidor para conectar con la base de datos
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+let URI = '';
 
+if (config.isProd) {
+  URI = config.dbUrl;
+} else {
+  //encode uri es para protejer esta informacion
+  const USER = encodeURIComponent(config.dbUser);
+  const PASSWORD = encodeURIComponent(config.dbPassword);
+  //usualmente la ruta que nos da un servidor para conectar con la base de datos
+  URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+}
 //connectionstring es un atributo que pool reconoce
 const pool = new Pool({ connectionString: URI });
 
