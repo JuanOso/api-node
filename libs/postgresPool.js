d91d6525c1462c73fab98ed278e90a990e2ccfe4
Juan Osorio <jcamilo994@hotmail.com>
2022-10-21 23:09:13 -0500

    dev dependecies 7

--- libs/postgresPool.js
+++ libs/postgresPool.js
@@ -3,7 +3,7 @@ const { config } = require('./../config/config');
 
 const options = {};
 if (config.isProd) {
-  options.connectionString = 'postgres://juan:admin123@localhost:5432/my_store';
+  options.connectionString = config.dbUrl;
   options.ssl = {
     rejectUnauthorized: false,
   };
--- libs/sequelize.js
+++ libs/sequelize.js
@@ -21,10 +21,7 @@ if (config.isProd) {
 }
 
 //en dialect le pasamos a que tipo de base de daytos nos vamos a conectar
-const sequelize = new Sequelize(
-  'postgres://juan:admin123@localhost:5432/my_store',
-  options
-);
+const sequelize = new Sequelize(config.dbUrl, options);
 
 setupModels(sequelize);
 
