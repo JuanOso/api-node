62175a06b5f885a4d38c8455e70718bbe858d918
Juan Osorio <jcamilo994@hotmail.com>
2022-10-22 00:36:48 -0500

    dev dependecies 9

--- db/migrations/20220919221105-create-user.js
+++ db/migrations/20220919221105-create-user.js
@@ -1,10 +1,10 @@
 'use strict';
-const { DataTypes, Sequelize } = require('sequelize');
-const { USER_TABLE, UserSchema } = require('../models/user.model');
+//const { DataTypes, Sequelize } = require('sequelize');
+/*const { USER_TABLE, UserSchema } = require('../models/user.model');
 const { CUSTOMER_TABLE, CustomerSchema } = require('../models/customer.model');
 const { CATEGORY_TABLE, CategorySchema } = require('../models/category.model');
 const { PRODUCT_TABLE, ProductSchema } = require('../models/product.model');
-const { ORDER_TABLE } = require('../models/order.model');
+const { ORDER_TABLE, OrderSchema } = require('../models/order.model');
 const {
   OrderProductSchema,
   ORDER_PRODUCT_TABLE,
@@ -16,31 +16,7 @@ module.exports = {
     await queryInterface.createTable(CUSTOMER_TABLE, CustomerSchema);
     await queryInterface.createTable(CATEGORY_TABLE, CategorySchema);
     await queryInterface.createTable(PRODUCT_TABLE, ProductSchema);
-    await queryInterface.createTable(ORDER_TABLE, {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: DataTypes.INTEGER,
-      },
-      customerId: {
-        field: 'customer_id',
-        allowNull: false,
-        type: DataTypes.INTEGER,
-        references: {
-          model: CUSTOMER_TABLE,
-          key: 'id',
-        },
-        onUpdate: 'CASCADE',
-        onDelete: 'SET NULL',
-      },
-      createdAt: {
-        allowNull: false,
-        type: DataTypes.DATE,
-        field: 'created_at',
-        defaultValue: Sequelize.NOW,
-      },
-    });
+    await queryInterface.createTable(ORDER_TABLE, OrderSchema);
     await queryInterface.createTable(ORDER_PRODUCT_TABLE, OrderProductSchema);
   },
 
@@ -52,9 +28,9 @@ module.exports = {
     await queryInterface.dropTable(ORDER_TABLE);
     await queryInterface.dropTable(ORDER_PRODUCT_TABLE);
   },
-};
+};*/
 
-/*'use strict';
+'use strict';
 
 const { UserSchema, USER_TABLE } = require('./../models/user.model');
 
@@ -66,4 +42,4 @@ module.exports = {
   async down(queryInterface) {
     await queryInterface.dropTable(USER_TABLE);
   },
-};*/
+};
