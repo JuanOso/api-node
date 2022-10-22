13fc5fce46ff64388e206878da569f8329f2ecf8
Juan Osorio <jcamilo994@hotmail.com>
2022-10-21 21:31:20 -0500

    fix error in connection

--- libs/postgresPool.js
+++ libs/postgresPool.js
@@ -3,16 +3,22 @@ const { config } = require('./../config/config');
 
 let URI = '';
 
+const options = {};
 if (config.isProd) {
-  URI = config.dbUrl;
+  (options.connectionString = config.dbUrl),
+    (options.ssl = {
+      rejectUnauthorized: false,
+    });
 } else {
   //encode uri es para protejer esta informacion
   const USER = encodeURIComponent(config.dbUser);
   const PASSWORD = encodeURIComponent(config.dbPassword);
   //usualmente la ruta que nos da un servidor para conectar con la base de datos
   URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+  options.connectionString = URI;
 }
+
 //connectionstring es un atributo que pool reconoce
-const pool = new Pool({ connectionString: URI });
+const pool = new Pool(options);
 
 module.exports = pool;
