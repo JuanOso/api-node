9fc04f12f0ea4f177adde5a6c90bf4c1c2a15871
Juan Osorio <jcamilo994@hotmail.com>
2022-10-21 23:06:29 -0500

    dev dependecies 6

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -9,10 +9,10 @@ const setupModels = require('./../db/models');
 
 const options = {
   dialect: 'postgres',
-  logging: 'production' ? false : true,
+  logging: config.isProd ? false : true,
 };
 
-if ('production') {
+if (config.isProd) {
   options.dialectOptions = {
     ssl: {
       rejectUnauthorized: false,
